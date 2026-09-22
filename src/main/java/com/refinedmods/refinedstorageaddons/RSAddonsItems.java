31db5afc95cf51fa8335d33e62811cfb437a417b
Furina-23 <Furina-23@users.noreply.github.com>
2026-09-22 12:47:03 +0800

    Update registration for Fabric registry adapter

--- src/main/java/com/refinedmods/refinedstorageaddons/RSAddonsItems.java
+++ src/main/java/com/refinedmods/refinedstorageaddons/RSAddonsItems.java
@@ -2,12 +2,12 @@ package com.refinedmods.refinedstorageaddons;
 
 import com.refinedmods.refinedstorageaddons.item.WirelessCraftingGridItem;
 import com.refinedmods.refinedstorage.registry.DeferredRegister;
-import com.refinedmods.refinedstorage.registry.ForgeRegistries;
+import com.refinedmods.refinedstorage.registry.RSRegistries;
 import com.refinedmods.refinedstorage.registry.RegistryObject;
 import net.minecraft.world.item.Item;
 
 public final class RSAddonsItems {
-    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RSAddons.ID);
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(RSRegistries.ITEMS, RSAddons.ID);
 
     static {
         WIRELESS_CRAFTING_GRID = ITEMS.register("wireless_crafting_grid", () -> new WirelessCraftingGridItem(WirelessCraftingGridItem.Type.NORMAL));
